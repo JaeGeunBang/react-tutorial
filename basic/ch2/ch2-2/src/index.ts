546bb28d1bf12a695358c385bc7aa8be7504d300
JaeGeunBang <jgb710@naver.com>
2021-09-04 16:58:42 +0900

    ch2-2, change, ramda 외부 패키지 사용

--- basic/ch2/ch2-2/src/index.ts
+++ basic/ch2/ch2-2/src/index.ts
@@ -1,11 +1,10 @@
 
 import IPerson from './person/IPerson'
 import Person, {makePerson} from './person/Person'
+import Change from 'chance'
+import * as R from 'ramda'
 
-const testMakePerson = () : void => {
-    let jane: IPerson = makePerson("Jane")
-    let jack: IPerson = new Person("Jack")
-    console.log(jane, jack)
-}
-
-testMakePerson()
+const change = new Change() // change 패키지는 가짜 데이터를 만들어줄떄 사용
+let persons: IPerson[] = R.range(0,2) // ramda는 함수형 유틸패키지
+    .map((n: number) => new Person(change.name(), change.age()))
+console.log(persons)
